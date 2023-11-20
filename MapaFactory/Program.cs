388245c0dc01ce18a1447c410ec35b82f6b8a73b
mikepalie <michalis.palierakis@gmail.com>
2023-11-20 23:44:26 +0200

    Factory

--- MapaFactory/Program.cs
+++ MapaFactory/Program.cs
@@ -10,6 +10,65 @@ namespace MapaFactory
     {
         static void Main(string[] args)
         {
+            string choice = "1";
+
+            if(choice == "1")
+            {
+                MasterCard m = new MasterCard();
+                m.GetCardType();
+            }
+            else if (choice == "2")
+            {
+                VisaCard v = new VisaCard();
+                v.GetCardType();
+            }
+            else if (choice == "3")
+            {
+                PaySafeCard p = new PaySafeCard();
+                p.GetCardType();
+            }
+
+
+
+        }
+    }
+
+    class MasterCard
+    {
+        public string GetCardType()
+        {
+            return "Master Card Credit Card";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+    }
+
+    class VisaCard
+    {
+        public string GetCardType()
+        {
+            return "Visa Card Credit Card";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+    }
+
+    class PaySafeCard
+    {
+        public string GetCardType()
+        {
+            return "Paysafe Card Credit Card";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 35000;
         }
     }
 }
---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICard card = Goofy.SimpleFactory(Card.MasterCard);
+            Console.WriteLine(  card.GetCardType()    );
+            Console.WriteLine(  card.GetCreditLimit()  );
+        }
+    }
+
+    enum Card
+    {
+        MasterCard,
+        VisaCard,
+        PaySafeCard
+    }
+
+    class Goofy
+    {
+        public static ICard SimpleFactory(Card choice)
+        {
+            switch (choice)
+            {
+                case Card.MasterCard: return new MasterCard();
+                case Card.VisaCard: return new VisaCard();
+                case Card.PaySafeCard: return new PaySafeCard();
+                default: return null;
+            }
+        }
+    }
+
+    public interface ICard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+    }
+
+    public class MasterCard: ICard
+    {
+        public string GetCardType()
+        {
+            return "Master Card Credit Card";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+    }
+
+    public class VisaCard: ICard
+    {
+        public string GetCardType()
+        {
+            return "Visa Card Credit Card";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+    }
+
+    public class PaySafeCard :ICard
+    {
+        public string GetCardType()
+        {
+            return "Paysafe Card Credit Card";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+    }
+}
